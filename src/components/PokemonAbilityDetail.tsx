1283efda3c22170eccf04da559d8c14045dc6666
Steven Cribbs <stevencribbs@msn.com>
2025-04-20 01:18:10 -0600

    aligned component name

---
+++ src/components/PokemonAbilityDetail.tsx
@@ -0,0 +1,28 @@
+import { TableRow, TableCell, capitalize } from '@mui/material';
+import { useGetAbilityDetailsQuery } from 'store/pokemonApiSlice';
+
+interface AbilityDetailProps {
+  name: string;
+  url: string;
+  index: number;
+}
+
+const PokemonAbilityDetail = ({ name, url, index }: AbilityDetailProps) => {
+  const { data: effect, isLoading: isLoadingEffect } =
+    useGetAbilityDetailsQuery(url);
+
+  return (
+    <TableRow
+      sx={{
+        backgroundColor: index % 2 === 0 ? '#f8f8f8' : '#fff'
+      }}
+    >
+      <TableCell sx={{ border: 0 }}>{capitalize(name)}</TableCell>
+      <TableCell sx={{ border: 0 }}>
+        {isLoadingEffect ? 'Loading...' : effect}
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default PokemonAbilityDetail;
